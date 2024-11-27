b741d1bb00325d6c3821f99402548ae3522a1687
Francois Daoust <fd@tidoust.net>
2024-11-27 18:11:53 +0100

    Drop unused value types class

--- src/ast.py
+++ src/ast.py
@@ -4,44 +4,6 @@ from typing import Literal
 from dataclasses import dataclass
 from .tokens import Token
 
-class Types(StrEnum):
-    # any types
-    ANY = 'any'
-
-    # boolean types
-    # Boolean value (major type 7, additional information 20 or 21).
-    BOOL = 'bool'
-
-    # numeric types
-    # An unsigned integer or a negative integer.
-    INT = 'int'
-    # An unsigned integer (major type 0).
-    UINT = 'uint'
-    # A negative integer (major type 1).
-    NINT = 'nint'
-    # One of float16, float32, or float64.
-    FLOAT = 'float'
-    # A number representable as an IEEE 754 half-precision float
-    FLOAT16 = 'float16'
-    # A number representable as an IEEE 754 single-precision
-    FLOAT32 = 'float32'
-    # A number representable as an IEEE 754 double-precision
-    FLOAT64 = 'float64'
-
-    # string types
-    # A byte string (major type 2).
-    BSTR = 'bstr'
-    # A byte string (major type 2).
-    BYTES = 'bytes'
-    # Text string (major type 3)
-    TSTR = 'tstr'
-    # Text string (major type 3)
-    TEXT = 'text'
-
-    # null types
-    NIL = 'nil'
-    NULL = 'null'
-
 class ParentNode:
     '''
     To allow re-serialization of the abstract syntax tree in a way that
