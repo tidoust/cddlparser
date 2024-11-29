ffe907ecccecc6da68c4d5152f04b21a8da4a8af
Francois Daoust <fd@tidoust.net>
2024-11-29 19:02:28 +0100

    Complete __init__.py to export useful modules

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
-__all__ = ['src']
+from .src.parser import Parser
+from .src.ast import Marker
+from .src.tokens import Token, Tokens
+from .src import ast
+
+def parse(string) -> ast.CDDLTree:
+    parser = Parser(string)
+    return parser.parse()
